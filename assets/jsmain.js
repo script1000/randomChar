91c5d34ff57b946274ddee5c2d963ee40ac6d388
script1000 <95744657+script1000@users.noreply.github.com>
2022-06-17 12:34:02 +0900

    a

--- assets/jsmain.js
+++ assets/jsmain.js
@@ -1,3 +1,5 @@
+// JSmain.js
+// Author: script1000
 "use strict";
 var jsmain = {};
 jsmain.getURL = function() {
@@ -11,4 +13,16 @@ jsmain.getOURL = function() {
 jsmain.getParam = function(param) {
     var url = location.search;
     return url;
+}
+jsmain.tpTo = function(url) {
+    var redirect = url;
+    window.location.href(redirect);
+}
+jsmain.bTT = function(bin) {
+    var bins = bin;
+    var text_decoder = new TextDecoder('utf-8');
+    var rsl = text_decoder.decode(Uint8Array.from(bins).buffer);
+    console.log('Bin to Text Decoded');
+    console.log(rsl);
+    return rsl;
 }
